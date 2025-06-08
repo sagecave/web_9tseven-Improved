c16b65ca7b476df0c585542787cb0232fc6e1467
”sagecave” <Andreassage@gmail.com>
2025-06-08 22:16:07 +0200

    product ser bedre

--- src/app/productPage/page.js
+++ src/app/productPage/page.js
@@ -4,7 +4,7 @@ import ProductsLayout from "../../../layouts/ProductLayout";
 
 export default function ProductPage() {
   return (
-    <section className="mt-10 grid col-start-2 col-end-3 ">
+    <section className="mt-30 grid col-start-2 col-end-3 ">
       <h1 className="text-HeaderSizeSmall text-main_black pb-[20]">ALL PRODUCTS</h1>
       <ProductsLayout>
         <ProductContainer />
