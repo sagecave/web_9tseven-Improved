fd61908a6b7020dea89fc7b36877d3cf9bf923e8
”sagecave” <Andreassage@gmail.com>
2025-06-08 16:13:04 +0200

    images work,

--- components/products/ProductCard.jsx
+++ components/products/ProductCard.jsx
@@ -1,10 +1,10 @@
 import Link from "next/link";
-
+import Image from "next/image";
 export default function ProductCards({ title, productImage, category, price, slugName }) {
   return (
-    <Link className="w-fit" key={slugName} href={`/productPage/${slugName}`}>
+    <Link className="w-fit " rel="preload" key={slugName} href={`/productPage/${slugName}`}>
       <div className="hover:shadow-md md:w-fit md:p-4">
-        <img src={productImage} alt={title} className="w-auto  object-contain  " />
+        <Image loading="eager" rel="preload" width={300} height={300} src={productImage} alt={title} className="w-auto  object-contain  " />
         <div className="pt-4 pr-3 pl-3 pb-4 ">
           <p className="text-HeaderSizeSmall font-bold text-main_black">{title}</p>
           <p className="text-ParagraphSize text-neutral_black">{category}</p>
