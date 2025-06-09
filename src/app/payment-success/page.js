6ab8a3b060f925cc6233a462065eb9780be601e2
”sagecave” <Andreassage@gmail.com>
2025-06-09 20:33:26 +0200

    h

--- src/app/payment-success/page.js
+++ src/app/payment-success/page.js
@@ -7,9 +7,9 @@ const paymentSuccessPage = ({ searchParams }) => {
   const { amount } = React.use(searchParams);
 
   return (
-    <section className="grid col-start-2 col-end-3 h-[80vh] mt-20">
+    <section className="grid col-start-2 col-end-3 h-[80vh] ">
       <div className="flex justify-center h-auto mt-10 mb-10">
-        <div className=" border-2 border-alternativ_white w-1/2 p-6 flex flex-col justify-center items-center rounded-lg shadow-lg">
+        <div className=" border-2 border-alternativ_white w-1/2 p-6 flex flex-col justify-center items-center rounded-lg shadow-lg mt-20">
           <h1 className="text-HeaderSizeSmall text-main_black pb-[20]">Payment Success</h1>
           <p className="text-HeaderSizeSmall text-main_black pb-[20]">You have successfully paid</p>
 
